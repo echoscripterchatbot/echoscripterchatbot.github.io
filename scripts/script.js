b86a3870186a38d153f2109d22b049673480fd06
Hayk Antonyan <hayka5367@gmail.com>
2024-04-13 17:46:32 +0400

    fixed bugs

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,21 @@
 import data, {quenstions} from "../api/info.js"
+import name from "./visual.js"
 import replace from '../homanishner/info.js'
 const textArea = document.querySelector("textarea");
 const activeChat = document.getElementById("chatwrite");
 const selectDiv = document.getElementById('select')
-const name = "EchoScripter";
+const generationElmProcessAnim  = document.getElementById("gen")
 const startSelectors = ['HI', 'Can you help me']
-const stack= [];
+const stack= [startSelectors];
 let functional = {f: false};
-textArea.addEventListener('input', (e) => {
-    if(e.currentTarget.value){
-        selectDiv.classList.add('write')
-    }
-    else{
-        selectDiv.classList.remove('write')
-    }
-})
+// textArea.addEventListener('input', (e) => {
+//     if(e.currentTarget.value){
+//         selectDiv.classList.add('write')
+//     }
+//     else{
+//         selectDiv.classList.remove('write')
+//     }
+// })
 textArea.addEventListener("keydown", (e) => {
     if(e.key === 'Enter'){
         e.preventDefault()
@@ -25,10 +26,13 @@ textArea.addEventListener("keydown", (e) => {
         sendingProcess(t)
     }
 })
+
+
 function sendChat(text){
     activeChat.innerHTML += `<div class='sender'><p>${text}</p></div>`
 }
-function sendingProcess(t){
+async function sendingProcess(t){
+    textArea.value = '';
     if(t === 'back' && stack.length){ //todo finded bug (it's fixed but not 100%)
         stack.pop();
         addSelector(stack[stack.length - 1] || []);
@@ -36,15 +40,19 @@ function sendingProcess(t){
         return;
     }
     sendChat(t);
-    chatToUser(generate(t.toLowerCase()));
-    textArea.value = '';
+    generationElmProcessAnim.classList.add("active")
+    await generate(t.toLowerCase()).then((t) => {
+        chatToUser(t);
+    }).catch((err) =>{
+        alert(err);
+    })
 }
 function chatToUser(text){
     if(text === ''){
         return;
     }
-    selectDiv.classList.remove('write')
-
+    // selectDiv.classList.remove('write')
+    generationElmProcessAnim.classList.remove("active")
     activeChat.innerHTML += `<div class='chat'><p>${text}</p></div>`
 }
 function textConverter(t){
@@ -57,48 +65,54 @@ function textConverter(t){
     }
     return t;
 }
-function generate(text){
-    const info = {'{text}' : text, '{name}' : name, '{randFunc}' : randomAnswer, '{stack}' : stack, '{addSelector}' : addSelector};
-    text = textConverter(text);
-    //const el = data[text];
-    let answer = '';
-    let lastEl;
-    quenstions.map((el) => {//select in quenstion words
-        if(text.indexOf(el) !== -1){
-            const infoElm = data[el];
-            lastEl = infoElm;
-            answer += randomAnswer(infoElm.answer) + ' ' + randomAnswer(infoElm.addRandSymbool) +' ';
-        }
-    })
-    const {call, f, func} = functional;
-    if(!answer){ //if answer is not found
-        if(f){
-            let callArg = call.map((el) => {
-                const elm = info[el];
-                if(elm){
-                    return elm;
+async function generate(text){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const info = {'{text}' : text, '{name}' : name, '{randFunc}' : randomAnswer, '{stack}' : stack, '{addSelector}' : addSelector};
+            text = textConverter(text);
+            //const el = data[text];
+            let answer = '';
+            let lastEl;
+            quenstions.map((el) => {//select in quenstion words
+                if(text.indexOf(el) !== -1){
+                    const infoElm = data[el];
+                    lastEl = infoElm;
+                    answer += randomAnswer(infoElm.answer) + ' ' + randomAnswer(infoElm.addRandSymbool) +' ';
                 }
-                return el;
             })
-            return func(...callArg) || '';
-        }
-        return "I can't understand please select quenstions :)";
-    }
-    functional.f = false;
-    function randomAnswer(answers){ //random gen text
-        if (!answers){
-            return '';
-        }
-        return answers[Math.floor(Math.random() * answers.length)];
-    }
-    stack.push(lastEl.select)
-    if(lastEl.functional){
-        functional.f = true;
-        functional.func = lastEl.function
-        functional.call = lastEl.call;
-    }
-    addSelector(lastEl.select)
-    return answer + '.';
+            const {call, f, func} = functional;
+            if(!answer){ //if answer is not found
+                if(f){
+                    let callArg = call.map((el) => {
+                        const elm = info[el];
+                        if(elm){
+                            return elm;
+                        }
+                        return el;
+                    })
+                    resolve(func(...callArg) || '');
+                }
+                resolve("I can't understand please select quenstions :)");
+            }
+            // functional.f = false;
+            function randomAnswer(answers){ //random gen text
+                if (!answers){
+                    return '';
+                }
+                return answers[Math.floor(Math.random() * answers.length)];
+            }
+            if(lastEl){
+                stack.push(lastEl.select)
+                if(lastEl.functional){
+                    functional.f = true;
+                    functional.func = lastEl.function
+                    functional.call = lastEl.call;
+                }
+                addSelector(lastEl.select)
+            }
+            resolve(answer + '.')
+        }, 2000)
+    })
 }
 function addSelector(arr){
     if(!Array.isArray(arr)){
---
+++ scripts/visual.js
@@ -0,0 +1,5 @@
+const name = "EchoScripter";
+document.getElementById("name").innerText = name;
+document.getElementById("namePlace").placeholder = "Сообщение " + name;
+document.title = name;
+export default name;
